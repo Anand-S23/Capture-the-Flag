26d11f38397d106536043039bae7574aec08c33d
Anand-S23 <anandsroopsingh23@gmail.com>
2020-01-10 19:11:05 -0500

    working auth system

--- auth.py
+++ auth.py
@@ -3,11 +3,11 @@ import bcrypt
 
 db = sqlite3.connect('auth.db')
 cursor = db.cursor()
-cursor.execute('CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT)')
-db.commit()
+#cursor.execute('CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT)')
+#db.commit()
 
 def create_user(username, password, password_conf):
-  cursor.execute("""SELECT username FROM users WHERE username=?""", (username))
+  cursor.execute("""SELECT username FROM users WHERE username=?""", (username,))
   exisiting_user = cursor.fetchone()
 
   if exisiting_user: 
@@ -16,21 +16,21 @@ def create_user(username, password, password_conf):
   else:
     if password == password_conf: 
       encoded = password.encode('utf-8')
-      final_password = bcrypt.hashpw(password, bcrypt.gensalt())
+      final_password = bcrypt.hashpw(encoded, bcrypt.gensalt())
 
-      cursor.execute("INSERT INTO users VALUES (?, ?)", (username, hpass))
+      cursor.execute("INSERT INTO users VALUES (?, ?)", (username, final_password))
       db.commit()
 
     else: 
       print('Passwords did not match. ')
 
 def authenticate_user(username, password):
-  cursor.execute("""SELECT username, password FROM users WHERE username=?""", (username))
+  cursor.execute("""SELECT username, password FROM users WHERE username=?""", (username,))
   user = cursor.fetchone()
 
   if user: 
-    encoded_pass = user[1].encode('utf-8')
-    if bcrypt.checkpw(encoded_pass, password):
+    encoded_pass = password.encode('utf-8')
+    if bcrypt.checkpw(encoded_pass, user[1]):
       print('You have logged on.')
     else:
       print('Passwords don\'t match.')
@@ -39,6 +39,6 @@ def authenticate_user(username, password):
 
 
   
-
+authenticate_user('hi', 'hi')
 
 
